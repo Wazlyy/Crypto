58295e8f395d59e2abfe28770d3da1798a2b22de
H4ltor <timothee.cognard@ynov.com>
2021-05-11 17:05:15 +0200

    [Update] auth user

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,15 @@
 import React from 'react'
-
+import app from '../firebase/firebase'
 
     const Home = () => {
   
         return( 
+          
            <div>
              <iframe src="https://coin360.com/widget/map.html" frameborder="0"
                   title="Coin360.com: Cryptocurrency Market State" ></iframe>
+          <h1>Home</h1>
+          <button onClick={() => app.auth().signOut()}>Sign out</button>
           </div>
          )
       }
--- src/components/Login.js
+++ src/components/Login.js
@@ -12,7 +12,7 @@ const Login = ({history}) => {
             await app
             .auth()
             .signInWithEmailAndPassword(email.value, password.value);
-            history.push("/login");
+            history.push('/login');
             } catch (error) {
                 alert(error);
             }
@@ -21,42 +21,26 @@ const Login = ({history}) => {
     const { currentUser } = useContext(AuthContext);
 
     if (currentUser) {
-        return <Redirect to="/login" />;
+        return <Redirect to='/' />;
     }
     
     return(
         
-    <div class="login-wrap">
-    <div class="login-html">
-        <input id="tab-1" type="radio" name="tab" class="sign-in" checked/><label for="tab-1" class="tab">Se connecter</label>
-        <div class="login-form" onSubmit={handleLogin}>
-            <div class="sign-in-htm">
-                <div class="group">
-                    <label for="user" class="label">Identifiant</label>
-                    <input id="user" type="text" class="input"/>
-                </div>
-                <div class="group">
-                    <label for="pass" class="label">Mot de passe</label>
-                    <input id="pass" type="password" class="input" data-type="password"/>
-                </div>
-                <div class="group">
-                    <input id="check" type="checkbox" class="check" checked/>
-                    <label for="check"><span class="icon"></span>Rester connecté</label>
-                </div>
-                <div class="group">
-                    <input type="submit" class="button" value="Se connecter"/>
-                </div>
-                <div class="hr"></div>
-                <div class="foot-lnk">
-                    <a class="btn-forgot" href="#forgot">Mot de passe oublié ?</a>
-                </div>
-            </div>
-        
-            
-        </div>
-    </div>
-</div>
-);
-};
+        <div>
+        <h1>Log in</h1>
+        <form onSubmit={handleLogin}>
+          <label>
+            Email
+            <input name="email" type="email" placeholder="Email" />
+          </label>
+          <label>
+            Password
+            <input name="password" type="password" placeholder="Password" />
+          </label>
+          <button type="submit">Log in</button>
+        </form>
+      </div>
+    );
+  };
 
 export default withRouter(Login);
