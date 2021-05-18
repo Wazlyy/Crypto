405838e291ee1e5c0c6c2099bafb69aced7dd87f
Wazlyy <antoineplay11@gmail.com>
2021-05-18 15:41:16 +0200

    unlog btn

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import app from '../firebase/firebase'
+
 
 
     const Home = () => {
@@ -7,6 +9,7 @@ import React from 'react'
            <div className="coin360">
              <iframe src="https://coin360.com/widget/map.html" frameborder="0"
                   title="Coin360.com: Cryptocurrency Market State" ></iframe>
+                  <button onClick={() => app.auth().signOut()}>Sign out</button>
           </div>
          )
       }
