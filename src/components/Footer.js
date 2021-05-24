ad648cf0b2f08e303bfeec7c07ba60d9145d1c7b
Wazlyy <antoineplay11@gmail.com>
2021-05-24 17:19:51 +0200

    fix btn sign out

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import app from '../firebase/firebase'
 
 
 const Footer = () => (
   <footer>
       <p class="copyright">Cryptochomeur.fr</p>
+      <button onClick={() => app.auth().signOut()}>Sign out</button>
   </footer>
 )
 
